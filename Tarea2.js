f11a91f59f2fdfb792de9fae6872928594cc27f2
jdambrosio013 <60763225+jdambrosio013@users.noreply.github.com>
2020-02-09 13:00:45 -0600

    Add files via upload

---
+++ Tarea2.js
@@ -0,0 +1,27 @@
+var mes=10;
+switch (mes){
+  case 1:
+  case 2:
+  case 12:
+     document.write("Es Invierno");
+    break;   
+  case 3:
+  case 4:
+  case 5:
+     document.write("Es Primavera");
+    break;
+  case 6:
+  case 7:
+  case 8:
+    document.write("Es Verano");
+   break
+  case 9:
+  case 10:
+  case 11:
+   document.write("Es OtoÃ±o");
+    break
+    default:
+    document.write("Verifique mes");
+}
+
+    document.write("16005017
